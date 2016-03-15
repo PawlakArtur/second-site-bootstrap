7c0ed344f49ebdcc1163ef31ce062b8facf77d74
Artur Pawlak <pawlak.artur90@gmail.com>
2016-03-15 20:53:15 +0100

    Added RWD

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,10 @@
-for(var i = 1; i < 3; i++){
+for(var i = 1; i < 5; i++){
 	(function (e) {
         var placeHolder = $("#placeholder-0" + e);
 		var input = $("#input-0" + e);
 
 		placeHolder.click(function(){
+			console.log("click");
 			input.focus();
 			placeHolder.animate({
 				top: '-20px',	
@@ -12,6 +13,7 @@ for(var i = 1; i < 3; i++){
 		});
 
 		input.focus(function(){
+			console.log("focus");
 			placeHolder.animate({
 				top: '-20px',	
 				fontSize: '0.8em'
@@ -19,6 +21,7 @@ for(var i = 1; i < 3; i++){
 		});
 
 		input.focusout(function(){
+			console.log("focus-out");
 			field = input.val();
 			if(field.length < 1)
 			{
